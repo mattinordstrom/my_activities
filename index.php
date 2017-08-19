8f068fe06a688a327dd799b9081ca4948a8ceaf3
Matti Nordstrom <matti.nordstrom@gmail.com>
2017-08-19 13:05:52 +0200

    Init commit of index and style

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<head>
+		<title>strava test</title>
+		<link rel="stylesheet" type="text/css" href="style.css">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+		<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<table>
+		<tr>
+			<td colspan="2">
+				<div class="bordered_div_head title_div">
+					<?php echo "NO WEBSITE TITLE SET";  ?>
+				</div>
+			</td>
+		</tr>
+		<tr><td></td></tr>
+		<tr>
+			<td colspan="2">
+				<div class="bordered_div_head summary_div">
+					<div class="summary_header_div">summary</div>
+					<div>NO DATA <br /> <br /> MONTHLY GOAL NOT SET</div>
+				</div>
+			</td>
+		</tr>
+		</table>
+		<div class="activity_data_container">
+		<table>
+		<tr>
+			<td width="300px">
+				<h1>running</h1>
+				Gear: UNKNOWN <br/><br/>
+				<div class="bordered_div_activity_content">
+					NO DATA
+				</div>
+			</td>
+			<td width="10px"></td>
+			<td width="300px" height="600px">
+				<h1>cycling</h1>
+				Gear: UNKNOWN <br/><br/>
+					<div class="bordered_div_activity_content">
+						NO DATA
+					</div>
+			</td>
+		</tr>
+	</table>
+</div>
+</body>
+</html>
