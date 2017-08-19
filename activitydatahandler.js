b54a14f96718ba35750d9225987301b3c90d0765
Matti Nordstrom <matti.nordstrom@gmail.com>
2017-08-19 21:52:01 +0200

    Added loading div and a bunch of data to the UI

--- activitydatahandler.js
+++ activitydatahandler.js
@@ -1,6 +1,6 @@
 var ActivityDataHandler = function(monthlyGoal) {
   this.monthlyGoal = monthlyGoal;
-  this.setMonthlyGoalInUI();
+
 };
 
 ActivityDataHandler.prototype.constructor = ActivityDataHandler;
@@ -10,21 +10,87 @@ ActivityDataHandler.prototype.setActivityData = function() {
     var athleteData = data.athlete;
     var activitiesData = data.activities;
 
-    var runningActivities = $.grep(activitiesData, function(e){ return e.type == "Run"; });
-    var cyclingActivities = $.grep(activitiesData, function(e){ return e.type == "Ride"; });
-
-    var lastRunningActivity = runningActivities[runningActivities.length-1].start_date;
-    var lastCyclingActivity = cyclingActivities[cyclingActivities.length-1].start_date;
-
-    $("#summaryName").text(athleteData.firstname + " " + athleteData.lastname);
-    $("#bike").text(athleteData.bikes[0].name);
-    $("#shoes").text(athleteData.shoes[0].name);
-
-    $("#running_content").html("Last activity: <br/>" + lastRunningActivity.slice(0,10));
-    $("#cycling_content").html("Last activity: <br/>" + lastCyclingActivity.slice(0,10));
-  });
+    this.updateUIWithFetchedData(athleteData, activitiesData);
+  }.bind(this));
 }
 
-ActivityDataHandler.prototype.setMonthlyGoalInUI = function() {
-  $("#monthlyGoal").text(this.monthlyGoal);
+ActivityDataHandler.prototype.updateUIWithFetchedData = function(athleteData, activitiesData) {
+  var runningActivities = $.grep(activitiesData, function(activity){ return activity.type == "Run"; });
+  var cyclingActivities = $.grep(activitiesData, function(activity){ return activity.type == "Ride"; });
+
+  this.setSummaryData(athleteData, activitiesData);
+
+  $("#bike").text(athleteData.bikes[0].name);
+  $("#shoes").text(athleteData.shoes[0].name);
+
+  this.setFiveLatest(runningActivities, cyclingActivities);
+
+  this.finishLoading();
+}
+
+ActivityDataHandler.prototype.setFiveLatest = function(runningActivities, cyclingActivities) {
+  var fiveLatestRunningActivities = runningActivities.splice(runningActivities.length-5,runningActivities.length);
+  var fiveLatestCyclingActivities = cyclingActivities.splice(cyclingActivities.length-5,cyclingActivities.length);
+  var runningContent = "Latest 5 activities: <br/><br/><br/>";
+  var cyclingContent = "Latest 5 activities: <br/><br/><br/>";
+  var i, activityDate, activityDistance, activityDurationMinutes, activityDurationSeconds;
+
+  for(i=4; i>=0; i--) {
+    if(fiveLatestRunningActivities[i]){
+      activityDate = fiveLatestRunningActivities[i].start_date.slice(0,10);
+      activityDistance = (fiveLatestRunningActivities[i].distance / 1000).toFixed(1);
+      activityDurationSeconds = fiveLatestRunningActivities[i].moving_time % 60;
+      activityDurationMinutes = (fiveLatestRunningActivities[i].moving_time - activityDurationSeconds) / 60
+
+      runningContent += activityDate + ": " + activityDistance + "km in " + activityDurationMinutes + ":" + activityDurationSeconds;
+      runningContent += "<br/><br/>";
+    }
+
+    if(fiveLatestCyclingActivities[i]){
+      activityDate = fiveLatestCyclingActivities[i].start_date.slice(0,10);
+      activityDistance = (fiveLatestCyclingActivities[i].distance / 1000).toFixed(1);
+      activityDurationSeconds = fiveLatestCyclingActivities[i].moving_time % 60;
+      activityDurationMinutes = (fiveLatestCyclingActivities[i].moving_time - activityDurationSeconds) / 60
+
+      cyclingContent += activityDate + ": " + activityDistance + "km in " + activityDurationMinutes + ":" + activityDurationSeconds;
+      cyclingContent += "<br/><br/>";
+    }
+  }
+  $("#running_content").html(runningContent);
+  $("#cycling_content").html(cyclingContent);
+}
+
+ActivityDataHandler.prototype.setSummaryData = function(athleteData, activitiesData) {
+  $("#summaryName").text(athleteData.firstname + " " + athleteData.lastname);
+
+  var now = new Date();
+  var currentDate = now.getDate();
+  var currentMonth = now.getMonth()+1;
+  var currentYear = now.getFullYear();
+
+  var activitiesThisYear = $.grep(activitiesData, function(activity){
+    return activity.start_date.split('-')[0] == currentYear;
+  });
+  var activitiesLastYear = $.grep(activitiesData, function(activity){
+    return activity.start_date.split('-')[0] == currentYear-1;
+  });
+
+  var activitiesThisMonth = $.grep(activitiesData, function(activity){
+    var isCurrentYear = activity.start_date.split('-')[0] == currentYear;
+    return isCurrentYear && activity.start_date.split('-')[1] == currentMonth;
+  });
+  var activitiesLastMonth = $.grep(activitiesData, function(activity){
+    var isCurrentYear = activity.start_date.split('-')[0] == currentYear;
+    return isCurrentYear && activity.start_date.split('-')[1] == currentMonth-1;
+  });
+
+  $("#activitiesThisYear").html("Total activities this year: " + activitiesThisYear.length + "/" + (this.monthlyGoal*12));
+  $("#activitiesLastYear").html("Total activities last year: " + activitiesLastYear.length + "/" + (this.monthlyGoal*12));
+
+  $("#activitiesThisMonth").html("Total activities this month: " + activitiesThisMonth.length + "/" + (this.monthlyGoal));
+  $("#activitiesLastMonth").html("Total activities last month: " + activitiesLastMonth.length + "/" + (this.monthlyGoal));
+}
+
+ActivityDataHandler.prototype.finishLoading = function() {
+  $("#loading").remove();
 }
